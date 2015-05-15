fc6b797929bc625302df18a292c07e679154fed9
Sergei.Ionin <sergei.ionin@reltio.com>
2015-05-15 20:24:40 +0300

    added tests for user management

--- src/main/java/com/github/sionin/StormpathClientWrapper.java
+++ src/main/java/com/github/sionin/StormpathClientWrapper.java
@@ -6,15 +6,16 @@ import com.stormpath.sdk.account.Accounts;
 import com.stormpath.sdk.api.ApiKey;
 import com.stormpath.sdk.api.ApiKeyList;
 import com.stormpath.sdk.api.ApiKeys;
-import com.stormpath.sdk.application.Application;
-import com.stormpath.sdk.application.ApplicationList;
-import com.stormpath.sdk.application.Applications;
+import com.stormpath.sdk.application.*;
 import com.stormpath.sdk.authc.AuthenticationRequest;
 import com.stormpath.sdk.authc.AuthenticationResult;
 import com.stormpath.sdk.authc.UsernamePasswordRequest;
 import com.stormpath.sdk.client.Client;
 import com.stormpath.sdk.client.ClientBuilder;
 import com.stormpath.sdk.client.Clients;
+import com.stormpath.sdk.directory.AccountStore;
+import com.stormpath.sdk.directory.Directory;
+import com.stormpath.sdk.directory.DirectoryStatus;
 import com.stormpath.sdk.impl.http.ServletHttpRequest;
 import com.stormpath.sdk.impl.oauth.authc.AccessTokenAuthenticationRequest;
 import com.stormpath.sdk.impl.util.Base64;
@@ -22,11 +23,12 @@ import com.stormpath.sdk.oauth.AccessTokenResult;
 import com.stormpath.sdk.oauth.OauthAuthenticationResult;
 import com.stormpath.sdk.oauth.OauthRequestAuthenticator;
 import com.stormpath.sdk.oauth.TokenResponse;
+import com.stormpath.sdk.resource.Resource;
 import org.apache.commons.lang3.tuple.Pair;
 import org.mockito.Mockito;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Iterator;
+import java.util.*;
 
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
@@ -46,6 +48,18 @@ public abstract class StormpathClientWrapper {
         if (baseUrl != null && !baseUrl.trim().isEmpty()) {
             clientBuilder.setBaseUrl(baseUrl.trim());
         }
+
+//        CacheManager cacheManager = Caches.newCacheManager()
+//                .withDefaultTimeToLive(10, TimeUnit.MINUTES) //general default
+//                .withDefaultTimeToIdle(10, TimeUnit.MINUTES) //general default
+//                .withCache(Caches.forResource(GroupList.class) //Application-specific cache settings
+//                        .withTimeToLive(10, TimeUnit.MINUTES)
+//                        .withTimeToIdle(10, TimeUnit.MINUTES))
+//                .withCache(Caches.forResource(Group.class) //Application-specific cache settings
+//                        .withTimeToLive(10, TimeUnit.MINUTES)
+//                        .withTimeToIdle(10, TimeUnit.MINUTES))
+//                .build(); //build the CacheManager
+//        clientBuilder.setCacheManager(cacheManager);
 
         client = clientBuilder.build();
         application = getApplication(System.getProperty("application"));
@@ -115,4 +129,109 @@ public abstract class StormpathClientWrapper {
     }
 
     protected abstract UserWrapper getUser(Account account);
+
+    public <T extends Resource> T getResource(String href, Class<T> aClass) {
+        return client.getResource(href, aClass);
+    }
+
+
+    public List<Directory> getDirectories() {
+        return getDirectories(Collections.<String>emptySet());
+    }
+
+    public List<Directory> getDirectories(Set<String> names) {
+
+        AccountStoreMappingList storeMappings = getAccountStoreMappings();
+        if (storeMappings == null) {
+            return Collections.emptyList();
+        }
+        Iterator<AccountStoreMapping> storeMappingIterator = storeMappings.iterator();
+        if (storeMappingIterator == null || !storeMappingIterator.hasNext()) {
+            return Collections.emptyList();
+        }
+
+        List<Directory> directories = iterateStoreMappings(names, storeMappings);
+        return directories;
+    }
+
+    private List<Directory> iterateStoreMappings(Set<String> names, AccountStoreMappingList storeMappings) {
+        List<Directory> directories = new ArrayList<Directory>();
+        long time = System.currentTimeMillis();
+
+        for (AccountStoreMapping storeMapping : storeMappings) {
+            AccountStore accountStore = getAccountStore(storeMapping);
+            if (accountStore instanceof Directory) {
+                Directory directory = (Directory) accountStore;
+                if (directory.getStatus() == DirectoryStatus.ENABLED
+                        && (names.isEmpty() || names.contains(directory.getName()))
+                        ) {
+                    directories.add(directory);
+                }
+            }
+        }
+
+//        System.out.println(String.format("exit: iterateStoreMappings([names, storeMappings]) = %d", System.currentTimeMillis() - time));
+
+        return directories;
+    }
+
+    private AccountStore getAccountStore(AccountStoreMapping storeMapping) {
+        long time = System.currentTimeMillis();
+
+        AccountStore accountStore = storeMapping.getAccountStore();
+
+//        System.out.println(String.format("exit: getAccountStore([storeMapping]) = %d", System.currentTimeMillis() - time));
+
+        return accountStore;
+    }
+
+    private AccountStoreMappingList getAccountStoreMappings() {
+        long time = System.currentTimeMillis();
+
+        AccountStoreMappingList accountStoreMappings = application.getAccountStoreMappings();
+
+//        System.out.println(String.format("exit: getAccountStoreMappings([]) = %d", System.currentTimeMillis() - time));
+
+        return accountStoreMappings;
+    }
+
+
+    public List<UserWrapper> getUsers(String customerId) {
+        return getUsers(Collections.singleton(customerId));
+    }
+
+    public List<UserWrapper> getUsers(Set<String> customerIds) {
+
+        AccountList accounts = getAccounts();
+        List<UserWrapper> users = iterateAccountList(customerIds, accounts);
+        return users;
+    }
+
+    private List<UserWrapper> iterateAccountList(Set<String> customerIds, AccountList accounts) {
+        long time = System.currentTimeMillis();
+
+        List<UserWrapper> users = new ArrayList<UserWrapper>();
+        if (accounts != null) {
+            for (Account account : accounts) {
+                if (customerIds == null || customerIds.isEmpty() || customerIds.contains(account.getDirectory().getName())) {
+                    users.add(getUser(account));
+                }
+            }
+        }
+
+//        System.out.println(String.format("exit: iterateAccountList([customerIds, accounts]) = %d", System.currentTimeMillis() - time));
+
+        return users;
+    }
+
+    private AccountList getAccounts() {
+        long time = System.currentTimeMillis();
+
+        AccountList accounts = application.getAccounts(Accounts.criteria().limitTo(100).withDirectory());
+//        AccountList accounts = getResource(application.getAccounts().getHref(), AccountList.class);
+
+//        System.out.println(String.format("exit: getAccounts([]) = %d", System.currentTimeMillis() - time));
+
+        return accounts;
+    }
 }
--- src/main/java/com/github/sionin/StormpathTest.java
+++ src/main/java/com/github/sionin/StormpathTest.java
@@ -1,6 +1,8 @@
 package com.github.sionin;
 
 import com.stormpath.sdk.account.Account;
+import com.stormpath.sdk.directory.CustomData;
+import com.stormpath.sdk.directory.Directory;
 import com.stormpath.sdk.group.Group;
 import com.stormpath.sdk.group.GroupList;
 import com.stormpath.sdk.oauth.TokenResponse;
@@ -9,40 +11,61 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 public class StormpathTest {
 
     private static final Logger logger = LoggerFactory.getLogger(StormpathTest.class);
 
-    public static final List<UserWrapper> allUsers = new ArrayList<UserWrapper>(1000);
-    public static final int E_COUNT = 10;
+    public static final List<Object> garbage = new ArrayList<Object>(1000);
+    public static final int E_COUNT = 1;
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Test get user info by access token without user groups iteration");
+        System.out.println("Test without user groups iteration");
         StormpathClientWrapper clientWrapper = new StormpathClientWrapper() {
             protected UserWrapper getUser(Account account) {
-                return getUserWithoutGroups(account);
+                return getUserWithoutGroups(this, account);
             }
         };
-        testClient(clientWrapper);
+        testGetAllDirectories(clientWrapper, 5);
+        testGetAllUsers(clientWrapper, 5);
+        testAuthorization(clientWrapper, 10);
+//
 
-        System.out.println("\n\n\nTest get user info by access token");
-        StormpathClientWrapper clientWrapper2 = new StormpathClientWrapper() {
-            protected UserWrapper getUser(Account account) {
-                return getFullUser(account);
-            }
-        };
-        testClient(clientWrapper2);
+//        System.out.println("\n\n\nTest full user info");
+//        StormpathClientWrapper clientWrapper2 = new StormpathClientWrapper() {
+//            protected UserWrapper getUser(Account account) {
+//                return getFullUser(this, account);
+//            }
+//        };
+//        testGetAllDirectories(clientWrapper2, 1);
+//        testGetAllUsers(clientWrapper2, 1);
+//        testAuthorization(clientWrapper2, 10);
+//
+//        System.out.println("\n\n\nTest full user info with groups cache");
+//        StormpathClientWrapper clientWrapper3 = new StormpathClientWrapper() {
+//            protected UserWrapper getUser(Account account) {
+//                return getUserWithCachedGroups(this, account);
+//            }
+//        };
+//        testGetAllDirectories(clientWrapper3, 5);
+//        testGetAllUsers(clientWrapper3, 5);
+//        testAuthorization(clientWrapper3, 10);
+
+
+
 
     }
 
 
-    private static void testClient(StormpathClientWrapper clientWrapper) throws Exception {
+    private static void testAuthorization(final StormpathClientWrapper clientWrapper, int testCount) throws Exception {
         String username = System.getProperty("username", "");
         String password = System.getProperty("password", "");
         Pair<UserWrapper, TokenResponse> authority = clientWrapper.authorize(username, password);
 
+        System.out.println("Test Authorization");
         System.out.println("User: " + authority.getKey());
         System.out.println("Token: " + authority.getValue().getAccessToken());
 
@@ -50,35 +73,83 @@ public class StormpathTest {
         final String accessToken = authority.getValue().getAccessToken();
 
         List<List<Long>> results = new ArrayList<List<Long>>();
-        for (int i = 0; i < 10; i++) {
-            results.add(profile(clientWrapper, sourceUser, accessToken));
+        for (int i = 0; i < testCount; i++) {
+            results.add(profile(new Callable() {
+                @Override
+                public Object call() throws Exception {
+                    return authorizeByToken(clientWrapper, sourceUser, accessToken);
+                }
+            }));
+            TimeUnit.SECONDS.sleep(1);
         }
 
-        System.out.println(" # count total   min   max  aver");
+        printResults(results);
+        garbage.clear();
+    }
+
+    private static void testGetAllUsers(final StormpathClientWrapper clientWrapper, int testCount) throws Exception {
+
+        System.out.println("Test get all users");
+        List<List<Long>> results = new ArrayList<List<Long>>();
+        for (int i = 0; i < testCount; i++) {
+            results.add(profile(new Callable<List<UserWrapper>>() {
+                @Override
+                public List<UserWrapper> call() throws Exception {
+                    return clientWrapper.getUsers(Collections.EMPTY_SET);
+                }
+            }));
+            TimeUnit.SECONDS.sleep(1);
+        }
+
+        printResults(results);
+        garbage.clear();
+    }
+
+    private static void testGetAllDirectories(final StormpathClientWrapper clientWrapper, int testCount) throws Exception {
+
+        System.out.println("Test get all directories");
+        List<List<Long>> results = new ArrayList<List<Long>>();
+        for (int i = 0; i < testCount; i++) {
+            results.add(profile(new Callable<List<Directory>>() {
+                @Override
+                public List<Directory> call() throws Exception {
+                    return clientWrapper.getDirectories();
+                }
+            }));
+            TimeUnit.SECONDS.sleep(1);
+        }
+
+        printResults(results);
+        garbage.clear();
+    }
+
+    private static void printResults(List<List<Long>> results) {
+        System.out.println(" # count  total    min    max   aver");
         for (int i = 0; i < results.size(); i++) {
             List<Long> times = results.get(i);
             Long total = times.remove(0);
             Long min = Collections.min(times);
             Long max = Collections.max(times);
 
-            System.out.println(String.format("%2d %5d %5d %5d %5d %5d", i, times.size(),
-                    total, min, max, total / times.size()));
+            System.out.println(String.format("%2d %5d %6d %6d %6d %6d",
+                    i, times.size(), total, min, max, total / times.size()));
         }
     }
 
-    private static List<Long> profile(StormpathClientWrapper clientWrapper, UserWrapper sourceUser, String accessToken) throws Exception {
+    private static List<Long> profile(Callable callable) throws Exception {
         List<Long> results = new ArrayList<Long>(E_COUNT + 1);
+        results.add(0L);
         long totalTime = System.currentTimeMillis();
         for (int i = 0; i < E_COUNT; i++) {
             long time = System.currentTimeMillis();
 
-            UserWrapper userWrapper = authorizeByToken(clientWrapper, sourceUser, accessToken);
+            Object value = callable.call();
 
             results.add(System.currentTimeMillis() - time);
-            allUsers.add(userWrapper);
+            garbage.add(value);
         }
         totalTime = System.currentTimeMillis() - totalTime;
-        results.add(0, totalTime);
+        results.set(0, totalTime);
         return results;
     }
 
@@ -88,7 +159,7 @@ public class StormpathTest {
         return user;
     }
 
-    private static UserWrapper getFullUser(Account account) {
+    private static UserWrapper getFullUser(StormpathClientWrapper clientWrapper, Account account) {
         Set<String> groups = new HashSet<String>();
         for (Group group : account.getGroups()) {
             groups.add(group.getName());
@@ -96,21 +167,57 @@ public class StormpathTest {
         return new UserWrapper(
                 account.getUsername(),
                 account.getEmail(),
-                new HashMap<String, Object>(account.getCustomData()),
+                new HashMap<String, Object>(getCustomData(clientWrapper, account)),
                 groups
         );
     }
 
-    private static UserWrapper getUserWithoutGroups(Account account) {
+    private static UserWrapper getUserWithoutGroups(StormpathClientWrapper clientWrapper, Account account) {
         Set<String> groups = new HashSet<String>();
         GroupList groupList = account.getGroups();
         assert groupList != null;
         return new UserWrapper(
                 account.getUsername(),
                 account.getEmail(),
-                new HashMap<String, Object>(account.getCustomData()),
+                new HashMap<String, Object>(getCustomData(clientWrapper, account)),
                 groups
         );
+    }
+
+    private static CustomData getCustomData(StormpathClientWrapper clientWrapper, Account account) {
+        return clientWrapper.getResource(account.getCustomData().getHref(), CustomData.class);
+    }
+
+    private static Map<String, Set<String>> groupsCache = new HashMap<String, Set<String>>();
+
+    private static UserWrapper getUserWithCachedGroups(StormpathClientWrapper clientWrapper, Account account) {
+        GroupList groupList = account.getGroups();
+        assert groupList != null;
+        String href = groupList.getHref();
+
+        Set<String> groups = getGroupsFromCache(clientWrapper, href);
+        return new UserWrapper(
+                account.getUsername(),
+                account.getEmail(),
+                new HashMap<String, Object>(getCustomData(clientWrapper, account)),
+                groups
+        );
+    }
+
+    private static Set<String> getGroupsFromCache(StormpathClientWrapper clientWrapper, String href) {
+        Set<String> groups = groupsCache.get(href);
+        if (groups != null) {
+            return groups;
+        }
+        groups = new HashSet<String>();
+
+        GroupList groupList = clientWrapper.getResource(href, GroupList.class);
+        for (Group group : groupList) {
+            groups.add(group.getName());
+        }
+
+        groupsCache.put(href, Collections.unmodifiableSet(groups));
+        return groups;
     }
 
 }
